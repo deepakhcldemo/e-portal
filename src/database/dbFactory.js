536e4b2e924a7d229c8fb88eeb2f133774e8c417
Madan Lalprajapati <madan.l@HCL.COM>
2019-04-02 19:58:18 +0530

    added database access layer

---
+++ src/database/dbFactory.js
@@ -0,0 +1,12 @@
+import firebase from './firebasedb';
+import iDB from './indexeddb';
+export default class dbFactory {
+    static create(dbName) {
+        switch (dbName) {
+            case 'firebase':
+                return firebase;
+            case 'indexeddb':
+                return iDB;
+        }
+    }
+}
