385c8bec02ecebfbed577ae97f25db2b469381a6
Ayush Mishra <ayush-m@HCL.COM>
2019-04-04 11:22:28 +0530

    table created for student

--- src/views/Classes/index.js
+++ src/views/Classes/index.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import Modal from 'react-responsive-modal';
 import Header from "../../components/layout/header/Header";
 import { connect } from 'react-redux';
+import classess from './styles.module.css';
+
+import firebase from '../../database/firebasedb';
 
 
 class Classes extends Component {
-  openModal = this.props.modalState;
+  state = {
+    student: []
+  };
 
+
+  componentDidMount() {
+    let tempData = [];
+    const ePortalDatabase = firebase.firestore();
+    ePortalDatabase.collection('users').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+
+        // this.setState({
+        //   Student: tempData
+        // })
+        tempData.push(doc.data());
+      })
+      this.setState({
+        student: tempData
+      })
+    });
+    
+  }
 
   onOpenModal = () => {
     this.setState({ open: true });
@@ -17,19 +40,40 @@ class Classes extends Component {
   };
 
   render() {
-    const style = {
-        backgroundColor : 'orange'
-    }
+    const studentList = this.state.student.map((student) => {
+      return (
+        <tr>
+          <td>{student.fname}</td>
+          <td>{student.lname}</td>
+          <td><input type ="checkbox"/></td>
+        </tr>
+      );
+    });
     const { modalState } = this.props;
     return (
       <div className="container-fluid">
+
         <Modal open={modalState} onClose={this.props.closeModal} center>
-          <h2>Create Class</h2>
+
           <form>
-          <div className="form-group">
-            <label htmlFor="classTxt">Class Name:</label>
-            <input type="text" id="classTxt" style = {style}  />
-          </div>
+            <h2 className={classess.color} >Create Class</h2>
+
+            <div>
+              <label htmlFor="classTxt" >Class Name:</label>
+              <input type="text" id="classTxt" className={classess.classTextBox} />
+              <table>
+                <thead>
+                  <tr><td>First Name</td>
+                  <td>Last Name</td>
+                  </tr>
+                  
+                </thead>
+                <tbody>
+                {studentList}
+                </tbody>
+              </table>
+
+            </div>
           </form>
         </Modal>
       </div>
