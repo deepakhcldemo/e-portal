149732e00e74ae28b76ead56e4e0d8489980dc50
imeravinash <krishnakumar_y@HCL.COM>
2019-04-08 11:02:32 +0530

    login page modified

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 class App extends Component {
   render() {
     return (
-      
       <div>
         {this.props.spinnerStatus ? (
           <div className="spinner-overlay-container">
@@ -38,19 +37,17 @@ class App extends Component {
             </div>
           </div>
         ) : null}
-        {/* <ReduxToastr
+        <ReduxToastr
           timeOut={4000}
           newestOnTop
           preventDuplicates
           position="bottom-center"
           transitionIn="fadeIn"
           transitionOut="fadeOut"
-        /> */}
+        />
 
-        
         <Switch>
           <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Registration} />
           <PrivateRoute path="/dashboard" component={Dashboard} />
           <PrivateRoute path="/profile" component={Profile} />
           <PrivateRoute path="/classes" component={Classes} />
