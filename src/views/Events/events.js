b62f735d8b29fd7ac795bc847f3429f112052767
Ayush Mishra <ayush-m@HCL.COM>
2019-04-09 10:07:33 +0530

    event create complete structure

--- src/views/Events/events.js
+++ src/views/Events/events.js
@@ -40,6 +40,7 @@ class CreateEvent extends Component {
     return (
       <div className="wrapper">
         <div className="row">
+        <div className ="back-to-dashborad"><i class="fa fa-angle-left left-arrow-icon"></i><span>Back To Dashboard</span></div>
           <div className="col-12 col-sm-12 col-md-12 col-lg-12">
             <h1>Create Event</h1>
             <div className="form-group">
@@ -54,7 +55,10 @@ class CreateEvent extends Component {
               <label>Event Description:</label>
               <textarea rows="4" cols="50" className="form-control"></textarea>
             </div>
-
+            <div className="input-container">
+              <label className="calender-label">Students :</label>
+              <input type="text" className="form-control" /> <i className="fa fa-search search-icon" aria-hidden="true"></i>
+            </div>
             <div className="form-group">
               <div className="row">
                 <div className="col-4 col-sm-12 col-md-4 col-lg-4">
@@ -84,13 +88,13 @@ class CreateEvent extends Component {
                       onChange={this.onChangeDate}
                     /> : null}
                 </div>
-                <div className ="form-group">
-                  <label className = "margin-both">All Day Event </label>
+                <div className="form-group">
+                  <label className="margin-both">All Day Event </label>
                   <input type="checkbox" name="checkbox" value="value" />
                 </div>
-                <div className ="form-group margin-top-bottom">
-                  <input type="button" className="btn btn-success margin-both " value ="Create Event" />
-                  <input type="button" className="btn btn-danger" value ="Cancel" />
+                <div className="form-group margin-top-bottom">
+                  <input type="button" className="btn btn-success margin-both " value="Create Event" />
+                  <input type="button" className="btn btn-danger" value="Cancel" />
                 </div>
               </div>
 
