97ce607c62e59ee8235888d1fb2e3b46909f9329
MD Shakir Hussain <MDShakirH@hcl.com>
2019-04-14 18:47:10 +0530

    added slider

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import Header from "../../components/layout/header/Header";
 import { connect } from "react-redux";
 import { resolve } from "q";
-import Carousel from "../../components/carousel/Carousel";
+import Carousel from 'react-bootstrap/Carousel';
+import Slider from '../../components/slider/Slider';
+
+// import Carousel from "../../components/carousel/Carousel";
 import Modal from "../../components/carousel/Modal";
 
 class Dashboard extends Component {
@@ -61,33 +64,64 @@ class Dashboard extends Component {
               <Header headeTitle="Dashboard" />
             </div>
           </div>
-
           <div className="row">
-            <div className="col-3">
-              <button className="btn btn-primary" onClick={this.createEvent}>
-                Create Event
-              </button>
-            </div>
-            <div className="col-9">
-              <div className="col-12 content-container">
-                {/* <button onClick ={this.createClass}>Create Class ...</button> */}
-                {/* <Classes></Classes> */}
-              </div>
+            <div className="col-12 main-wrapper">
+              <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://i.pinimg.com/originals/35/5d/65/355d65da2e1dc28b3399951765bc5fb1.jpg"
+                  alt="First slide"
+                />
+                {/* <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption> */}
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://i.pinimg.com/originals/35/5d/65/355d65da2e1dc28b3399951765bc5fb1.jpg"
+                  alt="Third slide"
+                />
+
+                {/* <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption> */}
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://i.pinimg.com/originals/35/5d/65/355d65da2e1dc28b3399951765bc5fb1.jpg"
+                  alt="Third slide"
+                />
+
+                {/* <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption> */}
+              </Carousel.Item>
+            </Carousel>
             </div>
           </div>
 
-          <div className="row">
-            <div className="col-12 content-container">
-              <Carousel
-                toggleModal={(imageType, imageSrc) =>
-                  this.toggleModal(imageType, imageSrc)
-                }
-              />
-              <Modal show={this.state.isOpen} onClose={this.toggleModalClose}>
-                {displayModalstring}
-              </Modal>
+          <div className="row dark-bg">
+            <div className="col-12">
+             
+              <Slider>
+                <h3 className="mt-30">Top <span>></span></h3>
+              </Slider>
+
+              <Slider>
+                <h3 className="mt-30">Newly added videos <span>></span></h3>
+              </Slider>
+              
             </div>
           </div>
+          
+
+          
         </div>
       </React.Fragment>
     );
