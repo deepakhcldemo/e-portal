e6474df62cae10ee21cb9a9e9c34d48ea069831b
Madan Lalprajapati <madan.l@HCL.COM>
2019-04-17 15:43:14 +0530

    header link

--- src/components/layout/header/HeaderHome.js
+++ src/components/layout/header/HeaderHome.js
@@ -5,12 +5,35 @@ import './HomeHeader.css';
 
 class HeaderHome extends Component {
   state = {
-    headeTitle: this.props.headeTitle ? this.props.headeTitle : "Default"
+    headeTitle: this.props.headeTitle ? this.props.headeTitle : "Default",
+    dashboardLink : '/home'
+  };
+  componentDidMount = () => {
+     const user = JSON.parse(localStorage.getItem('userProfile'));
+    if (user) {
+      if (user.role === 'Teacher') {
+        this.setState({ dashboardLink: '/teacher' });
+      } else if (user.role === 'Student') {
+        this.setState({ dashboardLink: '/student' });
+      } else {
+        this.setState({ dashboardLink: '/admin' });
+      }
+    } else {
+      this.setState({ dashboardLink: '/login' });
+    }
   };
   navigateTo = (linkName)=>{
     this.props.history.push(linkName);
   }
+
   render() {
+    let userLink;
+    const user = JSON.parse(localStorage.getItem('userProfile'));
+    if (user) {
+      userLink = <div className="home-header-nav-item home-header-nav-item--position"><a onClick={() => this.navigateTo(this.state.dashboardLink)} className="home-header-link">Dashboard</a></div>;
+    } else {
+      userLink = <div className="home-header-nav-item home-header-nav-item--position"><a onClick={() => this.navigateTo(this.state.dashboardLink)} className="home-header-link">SignIn/SignUp</a></div>;
+    }
     return (
       <header className="header-container">
         <div className="home-header-nav-item home-header-logo" style={{
@@ -21,8 +44,7 @@ class HeaderHome extends Component {
           backgroundSize: 'contain'
         }}></div>
         <div>
-        
-          <div className="home-header-nav-item home-header-nav-item--position"><a onClick={()=>this.navigateTo('/login')} className="home-header-link">SignIn/SignUp</a></div>
+          {userLink}
           <div className="home-header-nav-item home-header-nav-item--position"><a onClick={()=>this.navigateTo('/contactus')} className="home-header-link">Contact Us</a></div>
           <div className="home-header-nav-item home-header-nav-item--position"><a onClick={()=>this.navigateTo('/aboutus')} className="home-header-link">About Us</a></div>
         </div>
