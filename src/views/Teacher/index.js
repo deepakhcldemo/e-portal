9c3013226a82895ddc0c0da5055709b97f4b8550
Madan Lalprajapati <madan.l@HCL.COM>
2019-04-19 20:56:47 +0530

    layout fix

--- src/views/Teacher/index.js
+++ src/views/Teacher/index.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react'
 import HeaderHome from '../../components/layout/header/HeaderHome';
 import Navbar from './../../shared/components/Navbar'
@@ -7,11 +8,18 @@ import TopVideo from './TopVideo/TopVideo'
 import { TEACHER_DASHBOARD_LINKS } from './../../constant/Constant'
 import GLOBAL_VARIABLES from '../../config/config';
 import Slider from '../../components/slider/Slider';
+import Banner from '../../components/banner/Banner';
+import RecentVideo from '../../components/recentVideo/RecentVideo';
+import {
+    getBannerFromDB,
+    getCurriculumFromDB,
+    getNotificationFromDB
+} from './../../database/dal/firebase/curriculumDal';
 
 import './teacher.scss'
 
 const userList = [
-    { 
+    {
         uid: 'sdfdf',
         img: '',
         name: 'varun',
@@ -78,28 +86,81 @@ const videoDetails = [
     }
 ]
 class Teacher extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bannerRows: [],
+            carousellistNewlyItems: []
+        };
+    }
+
+    componentDidMount = () => {
+        getBannerFromDB().then(querySnapshot => {
+            let bannerData = [];
+            querySnapshot.forEach(doc => {
+                bannerData.push(doc.data());
+            });
+            this.setState({
+                bannerRows: bannerData
+            });
+        });
+
+        getCurriculumFromDB().then(querySnapshot => {
+            let currData = [];
+            querySnapshot.forEach(doc => {
+                currData.push(doc.data());
+            });
+
+            if (currData.length > 0) {
+                this.setState({
+                    carousellistNewlyItems: currData
+                });
+            }
+        });
+
+    };
+
     render = () => {
+        const {
+            bannerRows,
+            carousellistNewlyItems
+        } = this.state;
+
         return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 container--margin-bottom">
-                        <HeaderHome headeTitle="Teacher Dashboard" dashboardLinks={TEACHER_DASHBOARD_LINKS}/>
+                        <HeaderHome headeTitle="Teacher Dashboard" dashboardLinks={TEACHER_DASHBOARD_LINKS} />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12 main-wrapper"> 
-                        {/* <Slider listNewlyItems={listNewlyItems}>
-                            <h3 className="mt-30">{GLOBAL_VARIABLES.CATEGORYWISE_VIDEOS} <i className="fas fa-chevron-right"></i></h3>
-                        </Slider>  */}             
-                        <UserList heading="Students List" userList={userList} />   
+
+                {bannerRows.length > 0 && <Banner bannerRows={bannerRows} />}
+
+                {carousellistNewlyItems.length > 0 && (
+                    <RecentVideo carousellistNewlyItems={carousellistNewlyItems} headeTitle="Video Pending for Review" />
+                )}
+
+                {carousellistNewlyItems.length > 0 && (
+                    <RecentVideo carousellistNewlyItems={carousellistNewlyItems} headeTitle="Video Reviewed" />
+                )}
+
+                {carousellistNewlyItems.length > 0 && (
+                    <RecentVideo carousellistNewlyItems={carousellistNewlyItems} headeTitle="My Video" />
+                )}
+
+                {/* <div className="row">
+                    <div className="col-12 main-wrapper">
+                       
+                        <UserList heading="Students List" userList={userList} />
                         <TopVideo heading="Top 10 Videos" videoDetails={videoDetails} />
                     </div>
                 </div>
                 <div className="row">
-                    <Navbar links={TEACHER_DASHBOARD_LINKS}/>
-                </div>
+                    <Navbar links={TEACHER_DASHBOARD_LINKS} />
+                </div> */}
             </div>
         );
     }
 }
 export default Teacher
+
