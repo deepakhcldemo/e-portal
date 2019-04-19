650c441f0ec99ce534bbde5eb1c8bb06f43942ed
ERGarima <42827337+ERGarima@users.noreply.github.com>
2019-04-19 16:43:47 +0530

    notification popup modal from teacher details page1

--- src/shared/components/modalpopup/modalpopup.js
+++ src/shared/components/modalpopup/modalpopup.js
@@ -8,28 +8,56 @@ import {
   addStudentNames,
   addStudent
 } from './modalAction';
-
+import './modalpopup.css';
 class ModalPopUp extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      studentName : ''
+    }
   }
 
   onCloseModal = () => {
     this.props.closePopModal();
   };
-
+  componentDidMount() {
+    const studentDetails = JSON.parse(localStorage.getItem('userProfile'));
+    this.setState({
+      studentName : studentDetails.firstName + ' ' + studentDetails.lastName
+    })
+  }
   render() {
     const openModal = this.props.modalState;
-    debugger
+    
     return (
       <div>
-        <Modal open={openModal} onClose={this.onCloseModal} center />
+
+        <Modal open={openModal} onClose={this.onCloseModal} center>
+        <div className="header">
+          <h2>Create Notification</h2>
+        </div>
+        <div className="body">
+          <form>
+            <div className="form-group">
+            <div className ="teacher-student">
+            Student Name : {this.state.studentName}
+            </div>
+              <label htmlFor="email">Email address:</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+          </div>
+        </Modal>
+
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
+  console.log('state', state);
   return {
     modalState: state.teacherDetailsReducer.requestForReviewPop
   };
