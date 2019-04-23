465370df8833d614dc653b9c895913513b55de15
Santoshkumar Singh <santoshkumar-s@HCL.COM>
2019-04-23 14:16:37 +0530

    uploaded video thumb

--- src/components/topTutor/TopTutor.js
+++ src/components/topTutor/TopTutor.js
@@ -1,79 +1,21 @@
 import React, { Component } from 'react';
-
-import { Link } from 'react-router-dom';
+// Date format Plugin
+import moment from 'moment';
+// Slider Component Plugin
 import Slider from 'react-slick';
-import GLOBAL_VARIABLES from '../../config/config';
-import TeacherItem from '../../components/teacherItem/TeacherItem';
+// Link Plugin
+import { Link } from "react-router-dom";
+import TeacherItem from "../../components/teacherItem/TeacherItem";
 
 class TopTutor extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      noOfCarouselImage: ''
-    };
-  }
-
-  daysBetween(date1_seconds, date2_seconds) {
-    if (date1_seconds && date2_seconds) {
-      const one_day = 60 * 60 * 24 * 1000;
-      var difference_ms = Math.abs(date1_seconds - date2_seconds);
-      return Math.round(difference_ms / one_day);
-    } else {
-      return '';
-    }
-  }
-
   createChildren = records => {
     const carouselRows = records.map((carouselRecord, index) => {
-      const today = Date.now(); // convert into second
-      var noOfDays = this.daysBetween(
-        carouselRecord.createdAt ? carouselRecord.createdAt : today,
-        today
-      );
-      carouselRecord.noOfDays = noOfDays;
-      let title = '';
-      let profileImg = '';
-      let rating = '';
-
-      if (carouselRecord.firstName) {
-        title = carouselRecord.firstName;
-
-        if (carouselRecord.lastName) {
-          title += ' ' + carouselRecord.lastName;
-        }
-      } else if (carouselRecord.title) {
-        title = carouselRecord.title;
-      }
-
-      if (carouselRecord.profileImage) {
-        profileImg = carouselRecord.profileImage;
-      } else if (carouselRecord.thumb) {
-        profileImg = carouselRecord.thumb;
-      } else {
-        if (carouselRecord.gender === 'Female') {
-          profileImg =
-            'https://i0.wp.com/www.antgibbz.com/wp-content/uploads/2016/05/person-placeholder-200x200.jpg?ssl=1';
-        }
-        if (carouselRecord.gender === 'Male') {
-          profileImg =
-            'https://tricityescaperooms.com/wp-content/uploads/2018/01/person-placeholder-male-5.jpg';
-        }
-      }
-
-      if (carouselRecord.rating) {
-        rating = carouselRecord.rating;
-      }
-
-      if (carouselRecord.views) {
-        rating = carouselRecord.views;
-      }
-
+      carouselRecord.noOfDays = moment(carouselRecord.createdAt.toDate()).fromNow() 
       return (
         <div key={index} className="vd-wrapper  col-xs-12">
           <Link
             className="nav-link"
-            style={{ padding: '0px' }}
+            style={{ padding: "0px" }}
             to={`/home/teacher/${carouselRecord.userId}`}
             title={carouselRecord.name}
           >
@@ -87,11 +29,8 @@ class TopTutor extends Component {
   };
 
   render() {
-    const { carouselTop10Items } = this.props;
-    let title = '';
-    this.props.headeTitle
-      ? (title = this.props.headeTitle)
-      : (title = 'Top Tutor');
+    const { headeTitle, carouselTop10Items } = this.props;
+    const title = headeTitle ? headeTitle : "Top Tutor";
 
     const settingsTop10 = {
       dots: true,
@@ -134,10 +73,9 @@ class TopTutor extends Component {
         {carouselTop10Items.length > 0 && (
           <div className="col-12 content-container--background">
             <h3 className="mt-30">
-              {' '}
               {title} <i className="fas fa-chevron-right" />
             </h3>
-            <div style={{ background: '#FFF', textAlign: 'center' }}>
+            <div style={{ background: "#FFF", textAlign: "center" }}>
               <Slider {...settingsTop10}>
                 {this.createChildren(carouselTop10Items)}
               </Slider>
