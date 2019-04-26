627b439b522f48130e86acdfbe9c27f9b76e179e
Madan Lalprajapati <madan.l@HCL.COM>
2019-04-26 15:43:10 +0530

    notification layout

--- src/views/Notification/index.js
+++ src/views/Notification/index.js
@@ -1,115 +1,199 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import { connect } from "react-redux";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import Navbar from "./../../shared/components/Navbar";
+
 import HeaderHome from "../../components/layout/header/HeaderHome";
+
 import { getNotificationsFromDB } from "../../database/dal/firebase/studentDal";
-import { TEACHER_DASHBOARD_LINKS, STUDENT_DASHBOARD_LINKS } from './../../constant/Constant'
-import TeacherNotificationDetails from './../Teacher/Notification/notificationsDetails';
-import NotificationsDetails from './../Student/Notification/notificationsDetails';
+
+import TeacherNotificationDetails from "./../Teacher/Notification/notificationsDetails";
+
+import NotificationsDetails from "./../Student/Notification/notificationsDetails";
+
+import { setNotificationDetails } from "./action";
+import { TEACHER_DASHBOARD_LINKS } from "../../constant/Constant";
+import "./notification.css";
+
 // import Modal from 'react-responsive-modal'
 
 class Notification extends Component {
+  state = {
+    key: "chatNotification",
+    open: false,
+    userDetails: "",
+    notificationsList: ""
+  };
 
-    state = {
-        open: false,
-        userDetails: '',
-        notificationsList: ''
-    };
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
 
-    onOpenModal = () => {
-        this.setState({ open: true });
-    };
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
-    onCloseModal = () => {
-        this.setState({ open: false });
-    };
+  componentWillMount = () => {
+    this.setState({
+      userDetails: JSON.parse(localStorage.getItem("userProfile"))
+    });
+  };
 
-    componentWillMount = () => {
-        this.setState({
-            userDetails: JSON.parse(localStorage.getItem('userProfile'))
-        })
+  componentDidMount = () => {
+    const { userDetails } = this.state;
+    console.log(
+      "this.props.getNotificationMessage",
+      this.props.getNotificationMessage
+    );
+    if (this.props.getNotificationMessage === "NotificationDetails") {
+      this.setState({
+        key: "notification"
+      });
     }
-
-    componentDidMount = () => {
-        const { userDetails } = this.state;
-        getNotificationsFromDB(userDetails.userId, userDetails.role).onSnapshot(querySnapshot => {
-            let notificationsList = [];
-            querySnapshot.forEach(doc => {
-                notificationsList.push(Object.assign({ id: doc.id }, doc.data()));
-
-            });
-            this.setState({ notificationsList })
+    getNotificationsFromDB(userDetails.userId, userDetails.role).onSnapshot(
+      querySnapshot => {
+        let notificationsList = [];
+        querySnapshot.forEach(doc => {
+          notificationsList.push(Object.assign({ id: doc.id }, doc.data()));
         });
+        this.setState({ notificationsList });
+      }
+    );
+  };
 
-    };
+  notificationStatus = (notificationDetails, type) => {
+    const { userDetails } = this.state;
+    const classStatus =
+      notificationDetails.sstatus && notificationDetails.tstatus
+        ? "alert alert-success"
+        : notificationDetails.status && !notificationDetails.tstatus
+        ? "alert alert-warning"
+        : "alert alert-danger";
+    const userWiseStatus =
+      userDetails.role === "Teacher"
+        ? `Notification from  ${notificationDetails.sname}`
+        : `Notification from  ${notificationDetails.tname}`;
+    return type === "message" ? userWiseStatus : classStatus;
+  };
 
-    notificationStatus = (notificationDetails, type) => {
-        const { userDetails } = this.state
-        const classStatus = (notificationDetails.sstatus && notificationDetails.tstatus) ? 'alert alert-success' : (notificationDetails.status && !notificationDetails.tstatus) ? 'alert alert-warning' : 'alert alert-danger'
-        const userWiseStatus = (userDetails.role === 'Teacher') ? `Notification from  ${notificationDetails.sname}` : `Notification from  ${notificationDetails.tname}`;
-        return (type === 'message') ? userWiseStatus : classStatus
+  goToNotificationDetails = notification => {
+    this.props.setNotificationDetails(notification);
+    console.log(this.props);
+    this.props.history.push("/notificationDetails");
+  };
+
+  notificationStatus = (notificationDetails, type) => {
+    const { userDetails } = this.state;
+    let classStatus = "";
+    if (notificationDetails.sstatus && notificationDetails.tstatus) {
+      classStatus = "alert alert-success";
     }
 
-    render = () => {
-        const { notificationsList, userDetails } = this.state;
+    if (notificationDetails.sstatus && !notificationDetails.tstatus) {
+      classStatus = "alert alert-warning";
+    }
 
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12">
-                        <HeaderHome
-                            headeTitle="Teacher Dashboard"
-                            dashboardLinks={TEACHER_DASHBOARD_LINKS}
-                        />
-                    </div>
+    if (!notificationDetails.sstatus && !notificationDetails.tstatus) {
+      classStatus = "alert alert-danger";
+    }
+    const userWiseStatus =
+      userDetails.role === "Teacher"
+        ? `Notification from  ${notificationDetails.sname}`
+        : `Notification from  ${notificationDetails.tname}`;
+    return type === "message" ? userWiseStatus : classStatus;
+  };
+
+  goToNotificationDetails = notification => {
+    this.props.setNotificationDetails(notification);
+    console.log(this.props);
+    this.props.history.push("/notificationDetails");
+  };
+
+  render = () => {
+    const { notificationsList, userDetails } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <HeaderHome
+          headeTitle="Notification"
+          dashboardLinks={TEACHER_DASHBOARD_LINKS}
+        />
+
+        <div className="content-container">
+          <div className="col-12 col-md-12 col-xl-12 col-sm-12 col-lg-12">
+            <Tabs
+              id="tabs"
+              activeKey={this.state.key}
+              onSelect={key => this.setState({ key })}
+            >
+              <Tab eventKey="chatNotification" title="Chat Notification">
+                <div className="card notification-container">
+                  <div className="card-body notification-container">
+                    {userDetails.role === "Teacher" && (
+                      <TeacherNotificationDetails />
+                    )}
+                    {userDetails.role === "Student" && <NotificationsDetails />}
+                  </div>
                 </div>
-                <div className="row margin-bottom">
-                    <div className="col-12 col-md-12 col-xl-12 col-sm-12 col-lg-12">
-                        <div className="card">
-                            <div className="card-body">
-                                <h4>Notification</h4><hr />
-                                <ul className="nav nav-tabs" role="tablist">
-                                    <li className="nav-item">
-                                        <a className="nav-link" id="chat-notification-tab" data-toggle="tab" href="#chat-notification" role="tab" aria-controls="home" aria-selected="true">Chat Notifications</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" id="notification-tab" data-toggle="tab" href="#notification" role="tab" aria-controls="profile" aria-selected="false">Notifications</a>
-                                    </li>
-                                </ul>
-                                <div className="tab-content">
-                                    <div className="tab-pane fade show" id="chat-notification" role="tabpanel" aria-labelledby="chat-notification-tab">
-                                        {(userDetails.role === 'Teacher') && (
-                                            <TeacherNotificationDetails />
-                                        )}
-                                        {(userDetails.role === 'Student') && (
-                                            <NotificationsDetails />
-                                        )}
-                                    </div>
-                                    <div className="tab-pane fade" id="notification" role="tabpanel" aria-labelledby="notification-tab">
-                                        {notificationsList && notificationsList.map((notification, ind) => {
-                                            return (
-                                                <Link key={ind} to={`/notification/details/${notification.id}`}>
-                                                    <div className={this.notificationStatus(notification, 'classes')}>
-                                                        <div className="container">
-                                                            <b>Message:</b> {this.notificationStatus(notification, 'message')}
-                                                        </div>
-                                                    </div>
-                                                </Link>
-                                            )
-                                        }
-                                        )}
-                                    </div>
-                                </div>
+              </Tab>
+              <Tab eventKey="notification" title="Notification">
+                <div className="card notification-container">
+                  <div className="card-body notification-container">
+                    {notificationsList &&
+                      notificationsList.map((notification, ind) => {
+                        return (
+                          <div className="col-12" key={ind}>
+                            <div
+                              className={this.notificationStatus(
+                                notification,
+                                "classes"
+                              )}
+                              onClick={() =>
+                                this.goToNotificationDetails(notification)
+                              }
+                            >
+                              <div className="container">
+                                <b>Message:</b>{" "}
+                                {this.notificationStatus(
+                                  notification,
+                                  "message"
+                                )}
+                              </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row main-wrapper">
-                        <Navbar links={(userDetails.role === 'Teacher') ? TEACHER_DASHBOARD_LINKS : STUDENT_DASHBOARD_LINKS} />
-                    </div>
+                          </div>
+                        );
+                      })}
+                  </div>
                 </div>
-            </div>
-        );
-    }
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
+        <div className="col-12 main-wrapper">
+          <Navbar />
+        </div>
+      </div>
+    );
+  };
 }
-export default Notification
+const mapStateToProps = state => {
+  console.log("state", state);
+  return {
+    getNotificationMessage: state.notificationAcceptREducer.keyForNotification
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    setNotificationDetails: details => dispatch(setNotificationDetails(details))
+  };
+};
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Notification)
+);
