8ef6351051a680b6e13ae7fd8656d8612c075178
Ayush Mishra <ayush-m@HCL.COM>
2019-05-17 11:45:43 +0530

    removing chat initiate button

--- src/views/Notification/index.js
+++ src/views/Notification/index.js
@@ -198,7 +198,6 @@ class Notification extends Component {
               </Tab>
               <Tab eventKey="notification" title="Review Request">
               
-              <button>Initiate Chat</button>
                 <div className="card notification-container">
                   <div className="card-body notification-container">
                     <div className="filterByDate main-wrapper">
